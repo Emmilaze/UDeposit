a340de218618f2c5bf1c9278a6c5a4952a07f90f
Emmilaze <>
2020-03-09 12:08:33 +0200

    Next integration of developing
    Fixed styles

--- app/src/main/java/com/app/udeposits/core/UtilPercents.java
+++ app/src/main/java/com/app/udeposits/core/UtilPercents.java
@@ -3,12 +3,12 @@ package com.app.udeposits.core;
 public class UtilPercents {
     public static String getSimplePercents(int money, float percent, int days){
         float result = (money * percent * days)/(365 * 100);
-        return String.valueOf(result);
+        return String.format("%.2f", result);
     }
 
     public static String getDifficultPercents(int money, float percent, int days){
         int numberOfOperation = days/30;
         int result = (int) (money * Math.pow((1 + (percent * days)/(100*365)), numberOfOperation));
-        return String.valueOf(result);
+        return String.format("%.2f", result);
     }
 }
